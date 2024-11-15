1091279abf7176ccc2d3937fe8644916c096c19f
Vinicius Ferreira Citadin <viniciuscitadin@gmail.com>
2024-11-15 10:47:30 -0300

    implementada as rotas da api

--- index.js
+++ index.js
@@ -5,6 +5,33 @@ const db = require('./database');
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+app.get('/professionals/:id', async (req, res) => {
+    const professionals = await db.selectProfessional(req.params.id);
+    res.json(professionals);
+});
+
+app.get('/professionals', async (req, res) => {
+    const professionals = await db.selectProfessionals();
+    res.json(professionals);
+});
+
+app.post('/professionals', async (req, res) => {
+    await db.insertProfessional(req.body);
+    res.sendStatus(201);
+});
+
+app.put('/professionals/:id', async (req, res) => {
+    await db.updateProfessional(req.params.id, req.body);
+    res.sendStatus(200);
+});
+
+app.delete('/professionals/:id', async (req, res) => {
+    await db.deleteProfessional(req.params.id);
+    res.sendStatus(204);
+});
+
 app.listen(port, function () {
     console.log(`Executando na porta ${port}`);
 });
