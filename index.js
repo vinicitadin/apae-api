54c693a55593188c1f0a025a939a395e9386e651
Vinicius Ferreira Citadin <viniciuscitadin@gmail.com>
2024-11-15 11:22:11 -0300

    criado arquivos de rotas

--- index.js
+++ index.js
@@ -1,36 +1,11 @@
 require('dotenv').config();
 const port = process.env.PORT; 
-const db = require('./database');
+const routes = require('./src/routes');
 
 const express = require('express');
 const app = express();
 
-app.use(express.json());
-
-app.get('/professionals/:id', async (req, res) => {
-    const professionals = await db.selectProfessional(req.params.id);
-    res.json(professionals);
-});
-
-app.get('/professionals', async (req, res) => {
-    const professionals = await db.selectProfessionals();
-    res.json(professionals);
-});
-
-app.post('/professionals', async (req, res) => {
-    await db.insertProfessional(req.body);
-    res.sendStatus(201);
-});
-
-app.put('/professionals/:id', async (req, res) => {
-    await db.updateProfessional(req.params.id, req.body);
-    res.sendStatus(200);
-});
-
-app.delete('/professionals/:id', async (req, res) => {
-    await db.deleteProfessional(req.params.id);
-    res.sendStatus(204);
-});
+app.use('/', routes);
 
 app.listen(port, function () {
     console.log(`Executando na porta ${port}`);
