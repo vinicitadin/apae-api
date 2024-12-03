faba4339f934de6986c412a5d0678ad95d906f3b
Vinicius Ferreira Citadin <167343994+vinicitadin@users.noreply.github.com>
2024-12-03 19:29:22 -0300

    incluído headers

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const routes = require('./src/routes');
 const express = require('express');
 const app = express();
 
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+});
+
 app.use('/', routes);
 
 app.listen(port, function () {
