542530c4005bdb9a1e90b4c2530d35fa05f9bd7f
Vinicius Ferreira Citadin <167343994+vinicitadin@users.noreply.github.com>
2024-12-03 21:45:18 -0300

    atualizada rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,15 @@ const alunosRoutes = require('./alunosRoutes')
 const funcionariosRoutes = require('./funcionariosRoutes')
 const loginAlunosRoutes = require('./loginAlunosRoutes');
 const loginFuncionariosRoutes = require('./loginFuncionariosRoutes');
+const agendamentosRoutes = require('./agendamentosRoutes');
+const frequenciasRoutes = require('./frequenciaRoutes');
 
 router.use(express.json());
 router.use('/alunos', alunosRoutes);
 router.use('/funcionarios', funcionariosRoutes);
 router.use('/loginAlunos', loginAlunosRoutes);
 router.use('/loginFuncionarios', loginFuncionariosRoutes);
+router.use('/agendamentos', agendamentosRoutes);
+routes.use('/frequencias', frequenciasRoutes);
 
 module.exports = router;
