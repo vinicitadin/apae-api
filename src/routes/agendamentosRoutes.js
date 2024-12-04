d2e741f295960a97f1544ba935536f8fdc76e174
Vinicius Ferreira Citadin <viniciuscitadin@gmail.com>
2024-12-04 17:38:38 -0300

    corrigida chamada da função para o método GET

--- src/routes/agendamentosRoutes.js
+++ src/routes/agendamentosRoutes.js
@@ -7,40 +7,40 @@ router.get('/:id', async (req, res) => {
     res.json(agendamentos);
 });
 
-router.get('/:aluno', async (req, res) => {
-    const alunos = await db.selectAluno(req.params.aluno);
-    res.json(alunos);
-});
-
-router.get('/:funcionario', async (req, res) => {
-    const funcionarios = await db.selectFuncionario(req.params.funcionario);
-    res.json(funcionarios);
-});
-
-router.get('/:date', async (req, res) => {
-    const date = await db.selectDate(req.params.date);
-    res.json(date);
-});
-
-router.get('/:hora_inicio', async (req, res) => {
-    const time = await db.selectTime(req.params.time);
-    res.json(time);
-});
-
-router.get('/:hora_fim', async (req, res) => {
-    const time = await db.selectTime(req.params.time);
-    res.json(time);
-});
-
-router.get('/:tipo_atendimento', async (req, res) => {
-    const tipo = await db.selectTipo(req.params.tipo);
-    res.json(tipo);
-});
-
 router.get('/', async (req, res) => {
-    const agendamentos = await db.selectAgendamento();
+    const agendamentos = await db.selectAgendamentos();
     res.json(agendamentos);
 });
+
+// router.get('/:aluno', async (req, res) => {
+//     const alunos = await db.selectAluno(req.params.aluno);
+//     res.json(alunos);
+// });
+
+// router.get('/:funcionario', async (req, res) => {
+//     const funcionarios = await db.selectFuncionario(req.params.funcionario);
+//     res.json(funcionarios);
+// });
+
+// router.get('/:date', async (req, res) => {
+//     const date = await db.selectDate(req.params.date);
+//     res.json(date);
+// });
+
+// router.get('/:hora_inicio', async (req, res) => {
+//     const time = await db.selectTime(req.params.time);
+//     res.json(time);
+// });
+
+// router.get('/:hora_fim', async (req, res) => {
+//     const time = await db.selectTime(req.params.time);
+//     res.json(time);
+// });
+
+// router.get('/:tipo_atendimento', async (req, res) => {
+//     const tipo = await db.selectTipo(req.params.tipo);
+//     res.json(tipo);
+// });
 
 router.post('/', async (req, res) => {
     await db.insertAgendamento(req.body);
